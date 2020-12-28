b2916c7823fbf3af7980685c455fead429de07fe
Vitalii Kim <vitaliikim144@gmail.com>
2020-12-28 13:51:40 +0400

    refactoring

--- src/main/java/com/vkim/norfia/entity/UserDetailsEntity.java
+++ src/main/java/com/vkim/norfia/entity/UserDetailsEntity.java
@@ -1,32 +1,24 @@
 package com.vkim.norfia.entity;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users_details")
-public class UserDetailsEntity implements UserDetails {
+public class UserDetailsEntity extends LongIdEntity implements UserDetails {
 
-  @Id
-  private Long id;
-  private LocalDateTime lastUpdated;
   private String password;
   @OneToMany(mappedBy = "userDetails", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
   private Set<UserRolesEntity> userRoles;
@@ -54,26 +46,26 @@ public class UserDetailsEntity implements UserDetails {
 
   @Override
   public String getUsername() {
-    return null;
+    return user.getUserName();
   }
 
   @Override
   public boolean isAccountNonExpired() {
-    return false;
+    return accountNonExpired;
   }
 
   @Override
   public boolean isAccountNonLocked() {
-    return false;
+    return accountNonLocked;
   }
 
   @Override
   public boolean isCredentialsNonExpired() {
-    return false;
+    return credentialsNonExpired;
   }
 
   @Override
   public boolean isEnabled() {
-    return false;
+    return enabled;
   }
 }
